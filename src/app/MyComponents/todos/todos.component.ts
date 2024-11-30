d2058a95feb3cf3fce351846c376a11730b3a615
Abuzar Ch <abuzarc863@gmail.com>
2024-11-29 19:20:14 -0800

    changing

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      todos works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: './todos.component.html',
+  styleUrl: '[./todos.component.css]'
+
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  'todos':Todo[];
+constructor(){
+  this.todos = [
+    {
+      sno:1,
+      title:"This is Title",
+      desc: "Description",
+      active: true
+    },
+    {
+      sno:2,
+      title:"This is Title",
+      desc: "Description",
+      active: true
+    },
+    {
+      sno:3,
+      title:"This is Title",
+      desc: "Description",
+      active: true
+    },
+  ]
+}
+ngOnInit(): void {
+  
+}
 
 }
